3679229c958495a1ae073daa0cd1e2991cca1850
Alizamankhan786 <az030366@gmail.com>
2024-02-22 08:25:01 -0800

    index file created

---
+++ app.js
@@ -0,0 +1,46 @@
+alert("Hello to my web");
+
+
+alert("Error! please enter a valid password");
+
+
+
+alert("Welcome to js land Happy coding!");
+
+
+
+alert("Welcome to js land");
+
+
+// console.log(Hello.. i can run js through my web browsers console);
+
+
+
+
+
+var userName = "Ali";
+console.log(userName);
+var lastName = "zaman khan";
+console.log(lastName);
+
+
+alert("Ali Zaman Khan");
+
+
+alert("19 years old");
+
+
+alert("Certified mobile Application Development");
+
+
+
+
+
+alert("My email address is az030366@gmail.com");
+
+
+
+alert("I am trying to learn from the book A Smarter Way To Learn Javascript");
+
+
+alert("you have visited this site 10 Times");

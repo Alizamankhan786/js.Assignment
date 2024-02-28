bf520cb04646ff8a2b2ef2fca5b83269652efda6
Alizamankhan786 <az030366@gmail.com>
2024-02-28 12:59:33 -0800

    index file modified

--- app.js
+++ app.js
@@ -1,46 +1,194 @@
-alert("Hello to my web");
+// alert("Hello to my web");
 
 
-alert("Error! please enter a valid password");
-
-
-
-alert("Welcome to js land Happy coding!");
+// alert("Error! please enter a valid password");
 
 
 
-alert("Welcome to js land");
-
-
-// console.log(Hello.. i can run js through my web browsers console);
+// alert("Welcome to js land Happy coding!");
 
 
 
+// alert("Welcome to js land");
 
 
-var userName = "Ali";
-console.log(userName);
-var lastName = "zaman khan";
-console.log(lastName);
-
-
-alert("Ali Zaman Khan");
-
-
-alert("19 years old");
-
-
-alert("Certified mobile Application Development");
+// // console.log("Hello.. i can run js through my web browsers console");
 
 
 
 
 
-alert("My email address is az030366@gmail.com");
+// var userName = "Ali";
+// console.log(userName);
+// var lastName = "zaman khan";
+// console.log(lastName);
+
+
+// alert("Ali Zaman Khan");
+
+
+// alert("19 years old");
+
+
+// alert("Certified mobile Application Development");
 
 
 
-alert("I am trying to learn from the book A Smarter Way To Learn Javascript");
 
 
-alert("you have visited this site 10 Times");
+// alert("My email address is az030366@gmail.com");
+
+
+
+// alert("I am trying to learn from the book A Smarter Way To Learn Javascript");
+
+
+// alert("you have visited this site 10 Times");
+
+
+
+
+
+
+
+// var Number = +prompt("enter your Number")
+
+// if(Number > 3){
+//     console.log("Number is divisble by 3")
+// }else if (Number < 3){
+//     console.log("Number is not divisble by 3")
+// }
+
+
+
+
+// if(Number > 2){
+//     console.log("Input is an Odd Number")
+// }else if (Number < 2){
+//     console.log("Input is an Even Number")
+// }
+
+
+
+// var Age = +prompt("enter your Age")
+
+// if(Age > 18){
+//     console.log("Old Enough")
+// }else if (Age < 18){
+//     console.log("Too Young")
+// }
+
+
+// var Name = +prompt("enter your Name")
+
+// var Calculator = +prompt("enter your Calculator")
+
+
+// var Time = +prompt("enter your Time")
+
+
+// if(Time >= 10000 && Time < 1200){
+//     console.log("Good Morning")
+// }else if (Time >= 1200 && time < 1700){
+//     console.log("Good Afternoon")
+// }else if (Time >= 1700 && Time < 2100){
+//     console.log("Good Evening")
+// }else if (Time >= 2100 && Time <= 2359){
+//     console.log("Good Night")
+// }
+
+
+
+
+
+
+// var Year = +prompt("enter your Year")
+
+// if(Year > 2024){
+//     console.log("Leap year")
+// }else if (Year < 2024){
+//     console.log("Not a Leap year")
+// }
+
+
+
+
+
+// var Password = +prompt("enter your Password")
+
+
+// if (Password > 1){
+//     console.log("Correct password")
+// }else if (Password < 2){
+//     console.log("Incorrect Password")
+// }
+
+
+
+
+
+// var Name = +prompt("enter your Name")
+
+// var firstName = "Ali"
+// if(firstName==="Zaman"){
+//     console.log("Correct Name")
+// }else if (firstName = "Fahad"){
+//     console.log("Incorrect Name")
+// }
+
+
+// var Greeting = +prompt("Greeting")
+
+// var Hour = 13
+// if(Hour < 18 ){
+//     console.log("Good Day")
+// }else if (Hour > 18){
+//     console.log("Good Evening")
+// }
+
+
+
+// var Number = +prompt("Enter Your Number")
+
+// if(Number < 0){
+//     console.log("Number is negative")
+// }else if (Number > 10){
+//     console.log("Number is postive")
+// }else if (Number > 0){
+//     console.log("Number is Zero")
+// }
+
+
+
+var Month = +prompt("Enter Your Month")
+
+Month = 8;
+
+if (Month == 1){
+    alert("January");
+}else if (Month == 2){
+    alert("February");
+}else if (Month == 3){
+    alert("March");
+}else if (Month == 4){
+    alert("April");
+}else if (Month == 5){
+    alert("May");
+}else if (Month == 6){
+    alert("June");
+}else if (Month == 7){
+    alert("July");
+}else if (Month == 8){
+    alert("August");
+}else if (Month == 9){
+    alert("September");
+}else if (Month == 10){
+    alert("October");
+}else if (Month == 11){
+    alert("November");
+}else if (Month == 12){
+    alert("December");
+}else{
+    alert("Invalid Month");
+}
+
